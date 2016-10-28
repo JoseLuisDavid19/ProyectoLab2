1e9b902af04556e93f881b7e4864c5db40e7daf9
williamFportillo <portillowilliam18@gmail.com>
2016-10-28 08:39:39 -0600

    logcall

--- src/CallTG2/LogCall.java
+++ src/CallTG2/LogCall.java
@@ -5,10 +5,26 @@
  */
 package CallTG2;
 
+import java.util.Calendar;
+
 /**
  *
  * @author JoseLuis
  */
 public class LogCall {
+    
+    public int nmarcado;
+    public double duracion;
+    Calendar fecha; 
+    
+public LogCall(int nmarcado, double duracion){
+    this.nmarcado=54632736;
+    this.duracion=10;
+    this.fecha.getTime();
+}
+
+public String toString(){
+    return "Numero marcado: "+nmarcado+" Duracion de la llamada: "+duracion+" fecha actual: "+fecha;
+}
     
 }
